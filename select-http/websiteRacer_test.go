990d614b1d508010bd764dda5b942224a818994b
mathesukkj <kemuel.g7363@gmail.com>
2024-02-18 20:34:17 -0300

    feat: timeout if websites take more than 10 seconds to respond

--- select-http/websiteRacer_test.go
+++ select-http/websiteRacer_test.go
@@ -8,21 +8,36 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	defer slowServer.Close()
+	t.Run("test speed of two different websites", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		defer slowServer.Close()
 
-	fastServer := makeDelayedServer(0 * time.Millisecond)
-	defer fastServer.Close()
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("error if a website take > 10s to respond", func(t *testing.T) {
+		serverA := makeDelayedServer(12 * time.Second)
+		defer serverA.Close()
+
+		serverB := makeDelayedServer(11 * time.Second)
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Error("expected an error but didnt get one")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
