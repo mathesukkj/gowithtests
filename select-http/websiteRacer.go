ac1e018a3f8847616d48308d773437cd8ce2e93f
mathesukkj <kemuel.g7363@gmail.com>
2024-02-18 20:05:19 -0300

    refactor: refactoring tests + Racer function -- select-http

--- select-http/websiteRacer.go
+++ select-http/websiteRacer.go
@@ -1,25 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	startA := time.Now()
-	http.Get(a)
-	durationA := time.Since(startA)
-
-	fmt.Println(durationA)
-
-	startB := time.Now()
-	http.Get(b)
-	durationB := time.Since(startB)
-	fmt.Println(durationB)
+	durationA := measureResponseTime(a)
+	durationB := measureResponseTime(b)
 
 	if durationA < durationB {
 		return a
 	}
 	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }
