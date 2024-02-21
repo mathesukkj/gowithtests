620cc13c701a2550a9f4aba7dbec08ee6a999e34
mathesukkj <kemuel.g7363@gmail.com>
2024-02-20 21:30:28 -0300

    feat: add basic counter

---
+++ sync/sync_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, counter.Value(), 3)
+	})
+}
+
+func assertCounter(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
