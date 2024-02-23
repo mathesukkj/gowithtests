6678d0f50a3a75c41b4f1d89ffee33efc51057fb
mathesukkj <kemuel.g7363@gmail.com>
2024-02-22 22:38:19 -0300

    feat: make Fetch accept contexts, so our cancel signal is propagated properly -- context

--- context-server/context_test.go
+++ context-server/context_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -28,9 +29,30 @@ func (s *SpyStore) assertWasNotCancelled() {
 	}
 }
 
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
+
+	go func() {
+		var result string
+		for _, c := range s.response {
+			select {
+			case <-ctx.Done():
+				log.Println("spy store got cancelled")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(c)
+			}
+		}
+		data <- result
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
+	}
 }
 
 func (s *SpyStore) Cancel() {
