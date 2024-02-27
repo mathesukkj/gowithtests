219229eb5fabd9ca3d0cbfaf1812baf5854261f4
mathesukkj <kemuel.g7363@gmail.com>
2024-02-26 23:41:13 -0300

    feat: add hardcoded V

--- property-based-tests/romannumerals.go
+++ property-based-tests/romannumerals.go
@@ -6,6 +6,10 @@ func ConvertToRoman(num int) string {
 	var result strings.Builder
 
 	for i := num; i > 0; i-- {
+		if i == 5 {
+			result.WriteString("V")
+			break
+		}
 		if i == 4 {
 			result.WriteString("IV")
 			break
--- property-based-tests/romannumerals_test.go
+++ property-based-tests/romannumerals_test.go
@@ -12,6 +12,7 @@ func TestRomanNumerals(t *testing.T) {
 		{"2 converted to II", 2, "II"},
 		{"3 converted to III", 3, "III"},
 		{"4 converted to IV", 4, "IV"},
+		{"5 converted to V", 5, "V"},
 	}
 
 	for _, test := range cases {
