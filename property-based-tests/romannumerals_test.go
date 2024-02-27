dd5cb8fb7b52eb26187177e94be57ccfc3cf98f8
mathesukkj <kemuel.g7363@gmail.com>
2024-02-26 23:55:18 -0300

    feat: add functionality up to 1000

--- property-based-tests/romannumerals_test.go
+++ property-based-tests/romannumerals_test.go
@@ -1,35 +1,40 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
-		Description string
-		Arabic      int
-		Want        string
+		Arabic int
+		Roman  string
 	}{
-		{"1 converted to I", 1, "I"},
-		{"2 converted to II", 2, "II"},
-		{"3 converted to III", 3, "III"},
-		{"4 converted to IV", 4, "IV"},
-		{"5 converted to V", 5, "V"},
-		{"9 converted to IX", 9, "IX"},
-		{"10 converted to X", 10, "X"},
-		{"14 converted to XIV", 14, "XIV"},
-		{"18 converted to XVIII", 18, "XVIII"},
-		{"20 converted to XX", 20, "XX"},
-		{"39 converted to XXXIX", 39, "XXXIX"},
-		{"40 converted to XL", 40, "XL"},
-		{"47 converted to XXXIX", 47, "XLVII"},
-		{"49 converted to XLIX", 49, "XLIX"},
-		{"50 converted to L", 50, "L"},
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{9, "IX"},
+		{10, "X"},
+		{14, "XIV"},
+		{18, "XVIII"},
+		{20, "XX"},
+		{39, "XXXIX"},
+		{40, "XL"},
+		{47, "XLVII"},
+		{49, "XLIX"},
+		{50, "L"},
+		{100, "C"},
+		{900, "CM"},
+		{1984, "MCMLXXXIV"},
 	}
 
 	for _, test := range cases {
-		t.Run(test.Description, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%q converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
-			if got != test.Want {
-				t.Errorf("got %q want %q", got, test.Want)
+			if got != test.Roman {
+				t.Errorf("got %q want %q", got, test.Roman)
 			}
 		})
 	}
