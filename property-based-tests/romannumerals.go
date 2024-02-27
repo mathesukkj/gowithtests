0c9a9e0ea625cfaaa89d375cc0f00d1196287dbf
mathesukkj <kemuel.g7363@gmail.com>
2024-02-27 00:07:55 -0300

    feat: implement convertToArabic

--- property-based-tests/romannumerals.go
+++ property-based-tests/romannumerals.go
@@ -37,9 +37,14 @@ func ConvertToRoman(arabic int) string {
 }
 
 func ConvertToArabic(roman string) int {
-	total := 0
-	for range roman {
-		total++
+	var arabic int = 0
+
+	for _, numeral := range romanNumerals {
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			arabic += numeral.Value
+			roman = strings.TrimPrefix(roman, numeral.Symbol)
+		}
 	}
-	return total
+
+	return arabic
 }
